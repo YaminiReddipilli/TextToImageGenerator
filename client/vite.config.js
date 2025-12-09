4e70da9236ad91e55dba33bef7447c7e6104e834
YaminiReddipilli <yaminireddipilli@gmail.com>
2025-12-09 14:25:10 +0530

    Added Vite manifest config and production build

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: process.env.VITE_BASE_PATH || "/TextToImageGenerator",
+  build: {
+    outDir: "dist",
+    manifest: true,   // <-- REQUIRED so Express can load frontend
+  }
 })
